ceae4aebf4ffdd8d93f84ae1eb08d858c93f61da
LMenesesTroche <lucasmenesestroche@gmail.com>
2024-10-07 13:15:55 -0400

    Redux delete

--- Objetivos.js
+++ Objetivos.js
@@ -5,17 +5,16 @@
 //2.-Upload facil
 //3.-Autenticacion terceros google
 //4.-Pasarela de pagos paypal en upload no me saldra la opcion de subir fotos hasta pagar, luego de eso se habilita
-5.-delete
+// 5.-delete
 8.-Dashboard (Perdonar paga de un usuario, ver todos los pagos)
 6.-Comodo al usuario (Reactive)
-7.-Poder acomodar las imagenes
+7.- Muuuuy bonito
 9.- deploy
+7.-Poder acomodar las imagenes
 
 el boton profile de nav debe dirigir a mi portafolio
 Antes de que aparezca la imagen en upload debe salir una barra de carga
 
-! EN PROCESO DE BOTON DELETE
-Falta eliminarlo del cloudinary
 
 3X8SN3Q397XSL
 d48a552b-740b-4783-81f0-aa4b3e8d5c6f
--- client/src/redux/actions/users.js
+++ client/src/redux/actions/users.js
@@ -22,64 +22,66 @@ export const getAllUsers = () => {
 
 export const getUserInfoById = (id) => {
   return async (dispatch) => {
-    try{
+    try {
       let response = await axios.get(`${rutaBack}/users/${id}`);
       return dispatch({
         type: USER_PUBLIC_INFO,
         payload: response.data,
       });
-    }catch(error){
-      console.log("error en actions get user by id", error)
+    } catch (error) {
+      console.log("error en actions get user by id", error);
     }
-  }
-}
+  };
+};
 
-export const deletePhoto = (id_user, id_photo) => async (dispatch, getState) => {
-  try {
-    // Get the stored token from localStorage or another place
-    const token = localStorage.getItem('authToken');
+export const deletePhoto =
+  (id_user, id_photo) => async (dispatch, getState) => {
+    try {
+      // Get the stored token from localStorage or another place
+      const token = localStorage.getItem("authToken");
 
-    if (!token) {
-      throw new Error('No authorization token found');
-    }
+      if (!token) {
+        throw new Error("No authorization token found");
+      }
 
-    // console.log("TODO ESTO MANDO DESDE EL BACK PARA ELIMINAR id user:",id_user,"idPhoto", id_photo,"token:",token)
-    
-    const response = await axios.delete(
-      `${rutaBack}/photos/delete`, 
-      {
+      // console.log("TODO ESTO MANDO DESDE EL BACK PARA ELIMINAR id user:",id_user,"idPhoto", id_photo,"token:",token)
+
+      const response = await axios.delete(`${rutaBack}/photos/delete`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
         data: {
-          id_user, id_photo, 
-        }
+          id_user,
+          id_photo,
+        },
+      });
+
+      if (response.data.message === "Deleted successfully") {
+        
+        // Dispatch the delete action in Redux
+        dispatch({
+          type: "DELETE_PHOTO",
+          payload: id_photo,
+        });
       }
-    );
-
-    // // Dispatch the delete action in Redux
-    // dispatch({
-    //   type: 'DELETE_PHOTO',
-    //   payload: photoId,
-    // });
-
-  } catch (error) {
-    console.error('Error deleting photo:', error);
-  }
-};
+    } catch (error) {
+      console.error("Error deleting photo:", error);
+    }
+  };
 
 export const userHasPaidById = (id) => {
   return async (dispatch) => {
-    try{
-      let response = await axios.post(`${rutaBack}/users/hasPaid`,{userId:id});
+    try {
+      let response = await axios.post(`${rutaBack}/users/hasPaid`, {
+        userId: id,
+      });
       // console.log("Este es el response del back:",response.data)
       return dispatch({
         type: USER_HAS_PAID,
         payload: response.data,
       });
-      
-    }catch(error){
-      console.log("error en actions get user by id", error)
+    } catch (error) {
+      console.log("error en actions get user by id", error);
     }
-  }
-}
+  };
+};
