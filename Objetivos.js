5914ac4d84168ae7b26bc0bb960060f99af69576
LMenesesTroche <lucasmenesestroche@gmail.com>
2024-10-29 19:25:54 -0400

    notas

--- Objetivos.js
+++ Objetivos.js
@@ -9,13 +9,16 @@
 // 8.-Dashboard (Perdonar paga de un usuario, ver todos los pagos)
 //8.- Bloquear usuario desde el dashboard
 //9.- Poder editar tu nombre de usuario y foto de perfil 
-Bloquear todas las peticiones a un usuario bloqueado
-5.-Landing page
+?Bloquear todas las peticiones a un usuario bloqueado solo falta upload y delete photo, 
+?no cambiar toda la estructura de la db, solo cambiar nombrecito por fuera :/
+5.-Landing page (No mi usuario)
 7.- Muuuuy bonito
 6.-Comodo al usuario (Responsive)
 9.- deploy
-7.-Poder acomodar las imagenes
 
-si no pago que salga un cartelito en la seccion de perfil que diga que debe pagar para poder subir sus fotos 
-!Que el upload sea un modal ;)
+Poder acomodar las imagenes
+Que el upload sea un modal ;)
+Que el upload este en la pagina de perfil
+boton para copiar la direccion de tu portafolio 
+
 */
